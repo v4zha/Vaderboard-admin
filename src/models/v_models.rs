a46c01632747cbb10e8a424a350e12f10cc1fd46
v4zha <arvin_j4848@protonmail.com>
2023-05-18 12:23:20 +0530

    vaderEvent and event initialization

--- src/models/v_models.rs
+++ src/models/v_models.rs
@@ -5,38 +5,53 @@ use std::pin::Pin;
 use uuid::Uuid;
 
 pub type AsyncDbRes = Pin<Box<dyn Future<Output = Result<(), sqlx::Error>>>>;
+
 pub trait Player: Serialize {
     type DeserializedType: Deserialize<'static>;
     fn add_player(&self, db_pool: &SqlitePool) -> AsyncDbRes;
-    fn update_score(&self, db_pool: &SqlitePool) -> AsyncDbRes;
+    fn update_score(&mut self, points: i64, db_pool: &SqlitePool) -> AsyncDbRes;
+    fn new(name: String, logo: Option<String>) -> Self;
+    fn get_id(&self) -> Uuid;
+}
+
+pub trait VaderEvent {
+    type Participant: Player;
+
+    fn new<T: Player>(name: String, logo: Option<String>) -> Event<T> {
+        Event {
+            id: Uuid::new_v4(),
+            name,
+            logo,
+            players: Vec::<T>::new(),
+        }
+    }
+    fn add_event(&self) -> AsyncDbRes;
+    fn add_participant(&self, participant: Self::Participant) -> AsyncDbRes;
 }
 
 #[derive(Serialize, Deserialize)]
 pub struct Event<T: Player> {
-    id: Uuid,
-    name: String,
+    pub id: Uuid,
+    pub name: String,
     #[serde(default)]
-    logo: Option<String>,
-    players: Vec<T>,
+    pub logo: Option<String>,
+    pub players: Vec<T>,
 }
 
 #[derive(Serialize, Deserialize)]
 pub struct Team {
-    id: Uuid,
-    name: String,
-    score: i64,
+    pub id: Uuid,
+    pub name: String,
+    pub score: i64,
     #[serde(default)]
-    logo: Option<String>,
-    #[serde(default)]
-    desc: Option<String>,
-    members: Vec<User>,
+    pub logo: Option<String>,
+    pub members: Vec<User>,
 }
 #[derive(Serialize, Deserialize)]
 pub struct User {
-    id: Uuid,
+    pub id: Uuid,
+    pub name: String,
+    pub score: i64,
     #[serde(default)]
-    logo: Option<String>,
-    name: String,
-    #[serde(default)]
-    score: i64,
+    pub logo: Option<String>,
 }
--- src/services/db_services.rs
+++ src/services/db_services.rs
@@ -1,22 +1,69 @@
 use sqlx::SqlitePool;
+use uuid::Uuid;
 
-use crate::models::v_models::{AsyncDbRes, Player, Team, User};
+use crate::models::v_models::{AsyncDbRes, Event, Player, Team, User, VaderEvent};
 
 impl Player for User {
     type DeserializedType = Self;
+    fn new(name: String, logo: Option<String>) -> Self {
+        Self {
+            id: Uuid::new_v4(),
+            name,
+            logo,
+            score: 0,
+        }
+    }
     fn add_player(&self, db_pool: &SqlitePool) -> AsyncDbRes {
         Box::pin(async move { unimplemented!() })
     }
-    fn update_score(&self, db_pool: &SqlitePool) -> AsyncDbRes {
+    fn update_score(&mut self, points: i64, db_pool: &SqlitePool) -> AsyncDbRes {
+        self.score += points;
         Box::pin(async move { unimplemented!() })
+    }
+    fn get_id(&self) -> Uuid {
+        self.id
     }
 }
 impl Player for Team {
     type DeserializedType = Self;
+    fn new(name: String, logo: Option<String>) -> Self {
+        Self {
+            id: Uuid::new_v4(),
+            name,
+            logo,
+            members: Vec::new(),
+            score: 0,
+        }
+    }
     fn add_player(&self, db_pool: &SqlitePool) -> AsyncDbRes {
         Box::pin(async move { unimplemented!() })
     }
-    fn update_score(&self, db_pool: &SqlitePool) -> AsyncDbRes {
+    fn update_score(&mut self, points: i64, db_pool: &SqlitePool) -> AsyncDbRes {
+        self.score += points;
+        Box::pin(async move { unimplemented!() })
+    }
+    fn get_id(&self) -> Uuid {
+        self.id
+    }
+}
+
+impl VaderEvent for Event<Team> {
+    type Participant = Team;
+    fn add_participant(&self, participant: Self::Participant) -> AsyncDbRes {
+        Box::pin(async move { unimplemented!() })
+    }
+
+    fn add_event(&self) -> AsyncDbRes {
+        Box::pin(async move { unimplemented!() })
+    }
+}
+
+impl VaderEvent for Event<User> {
+    type Participant = User;
+    fn add_participant(&self, participant: Self::Participant) -> AsyncDbRes {
+        Box::pin(async move { unimplemented!() })
+    }
+    fn add_event(&self) -> AsyncDbRes {
         Box::pin(async move { unimplemented!() })
     }
 }
