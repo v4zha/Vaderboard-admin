52149113f5e65835f5aa49a252cda25f6f71c94e
v4zha <arvin_j4848@protonmail.com>
2023-05-18 19:57:01 +0530

    Event and event addition

--- src/models/v_models.rs
+++ src/models/v_models.rs
@@ -1,41 +1,46 @@
 use serde::{Deserialize, Serialize};
-use sqlx::SqlitePool;
+use sqlx::{Error, SqlitePool};
 use std::future::Future;
+use std::marker::PhantomData;
 use std::pin::Pin;
 use uuid::Uuid;
 
-pub type AsyncDbRes = Pin<Box<dyn Future<Output = Result<(), sqlx::Error>>>>;
+pub type AsyncDbRes<'a> = Pin<Box<dyn Future<Output = Result<(), Error>> + Send + 'a>>;
 
-pub trait Player: Serialize {
+pub trait Player<'a>: Serialize {
     type DeserializedType: Deserialize<'static>;
-    fn add_player(&self, db_pool: &SqlitePool) -> AsyncDbRes;
-    fn update_score(&mut self, points: i64, db_pool: &SqlitePool) -> AsyncDbRes;
+    fn add_player(&'a self, db_pool: &'a SqlitePool) -> AsyncDbRes<'a>;
+    fn update_score(&'a mut self, points: i64, db_pool: &'a SqlitePool) -> AsyncDbRes<'a>;
     fn new(name: String, logo: Option<String>) -> Self;
     fn get_id(&self) -> Uuid;
+    fn get_logo(&self) -> String;
 }
 
-pub trait VaderEvent {
-    type Participant: Player;
+pub trait VaderEvent<'a> {
+    type Participant: Player<'a>;
 
-    fn new<T: Player>(name: String, logo: Option<String>) -> Event<T> {
+    fn new<T: Player<'a>>(name: String, logo: Option<String>) -> Event<'a, T> {
         Event {
             id: Uuid::new_v4(),
             name,
             logo,
             players: Vec::<T>::new(),
+            marker: PhantomData,
         }
     }
-    fn add_event(&self) -> AsyncDbRes;
+    fn add_event(&'a self, db_pool: &'a SqlitePool) -> AsyncDbRes<'a>;
     fn add_participant(&self, participant: Self::Participant) -> AsyncDbRes;
+    fn get_logo(&self) -> String;
 }
 
 #[derive(Serialize, Deserialize)]
-pub struct Event<T: Player> {
+pub struct Event<'a, T: Player<'a>> {
     pub id: Uuid,
     pub name: String,
     #[serde(default)]
     pub logo: Option<String>,
     pub players: Vec<T>,
+    marker: PhantomData<&'a T>,
 }
 
 #[derive(Serialize, Deserialize)]
--- src/services/db_services.rs
+++ src/services/db_services.rs
@@ -3,7 +3,7 @@ use uuid::Uuid;
 
 use crate::models::v_models::{AsyncDbRes, Event, Player, Team, User, VaderEvent};
 
-impl Player for User {
+impl<'a> Player<'a> for User {
     type DeserializedType = Self;
     fn new(name: String, logo: Option<String>) -> Self {
         Self {
@@ -13,18 +13,21 @@ impl Player for User {
             score: 0,
         }
     }
-    fn add_player(&self, db_pool: &SqlitePool) -> AsyncDbRes {
+    fn add_player(&'a self, _db_pool: &'a SqlitePool) -> AsyncDbRes<'a> {
         Box::pin(async move { unimplemented!() })
     }
-    fn update_score(&mut self, points: i64, db_pool: &SqlitePool) -> AsyncDbRes {
+    fn update_score(&'a mut self, points: i64, _db_pool: &'a SqlitePool) -> AsyncDbRes<'a> {
         self.score += points;
         Box::pin(async move { unimplemented!() })
     }
     fn get_id(&self) -> Uuid {
         self.id
     }
+    fn get_logo(&self) -> String {
+        self.logo.as_ref().unwrap_or(&String::new()).to_string()
+    }
 }
-impl Player for Team {
+impl<'a> Player<'a> for Team {
     type DeserializedType = Self;
     fn new(name: String, logo: Option<String>) -> Self {
         Self {
@@ -35,35 +38,72 @@ impl Player for Team {
             score: 0,
         }
     }
-    fn add_player(&self, db_pool: &SqlitePool) -> AsyncDbRes {
+    fn add_player(&'a self, _db_pool: &'a SqlitePool) -> AsyncDbRes<'a> {
         Box::pin(async move { unimplemented!() })
     }
-    fn update_score(&mut self, points: i64, db_pool: &SqlitePool) -> AsyncDbRes {
+    fn update_score(&'a mut self, points: i64, _db_pool: &'a SqlitePool) -> AsyncDbRes<'a> {
         self.score += points;
         Box::pin(async move { unimplemented!() })
     }
     fn get_id(&self) -> Uuid {
         self.id
     }
+    fn get_logo(&self) -> String {
+        self.logo.as_ref().unwrap_or(&String::new()).to_string()
+    }
 }
 
-impl VaderEvent for Event<Team> {
+impl<'a> VaderEvent<'a> for Event<'a, Team> {
     type Participant = Team;
-    fn add_participant(&self, participant: Self::Participant) -> AsyncDbRes {
+    fn add_participant(&self, _participant: Self::Participant) -> AsyncDbRes {
         Box::pin(async move { unimplemented!() })
     }
 
-    fn add_event(&self) -> AsyncDbRes {
-        Box::pin(async move { unimplemented!() })
+    fn add_event(&'a self, _db_pool: &'a SqlitePool) -> AsyncDbRes<'a> {
+        let logo = self.get_logo();
+        let id = self.id.to_string();
+        let name = &self.name;
+        Box::pin(async move {
+            sqlx::query!(
+                "INSERT INTO events (id,name,logo,event_type) VALUES (?,?,?,?)",
+                id,
+                name,
+                logo,
+                "team_event"
+            )
+            .execute(_db_pool)
+            .await
+            .map(|_f| ())
+        })
+    }
+    fn get_logo(&self) -> String {
+        self.logo.as_ref().unwrap_or(&String::new()).to_string()
     }
 }
 
-impl VaderEvent for Event<User> {
+impl<'a> VaderEvent<'a> for Event<'a, User> {
     type Participant = User;
-    fn add_participant(&self, participant: Self::Participant) -> AsyncDbRes {
+    fn add_participant(&self, _participant: Self::Participant) -> AsyncDbRes {
         Box::pin(async move { unimplemented!() })
     }
-    fn add_event(&self) -> AsyncDbRes {
-        Box::pin(async move { unimplemented!() })
+    fn add_event(&'a self, _db_pool: &'a SqlitePool) -> AsyncDbRes<'a> {
+        let logo = self.get_logo();
+        let id = self.id.to_string();
+        let name = &self.name;
+        Box::pin(async move {
+            sqlx::query!(
+                "INSERT INTO events (id,name,logo,event_type) VALUES (?,?,?,?)",
+                id,
+                logo,
+                name,
+                "individual_event"
+            )
+            .execute(_db_pool)
+            .await
+            .map(|_f| ())
+        })
+    }
+    fn get_logo(&self) -> String {
+        self.logo.as_ref().unwrap_or(&String::new()).to_string()
     }
 }
