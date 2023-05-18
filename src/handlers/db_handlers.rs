4b6498dc0855791f2dd6925efed413e6e5069b76
v4zha <arvin_j4848@protonmail.com>
2023-05-18 11:33:13 +0530

    db-update

--- src/handlers/db_handlers.rs
+++ src/handlers/db_handlers.rs
@@ -1,4 +1,1 @@
-use crate::models::vader_models::Event;
-use actix_web::{web, Responder};
 
-pub fn add_event()
--- src/services/db_services.rs
+++ src/services/db_services.rs
@@ -5,23 +5,18 @@ use crate::models::v_models::{AsyncDbRes, Player, Team, User};
 impl Player for User {
     type DeserializedType = Self;
     fn add_player(&self, db_pool: &SqlitePool) -> AsyncDbRes {
-        Box::pin(async move {
-            unimplemented!()
-        })
+        Box::pin(async move { unimplemented!() })
     }
     fn update_score(&self, db_pool: &SqlitePool) -> AsyncDbRes {
-        Box::pin(async move {})
-            unimplemented!()
+        Box::pin(async move { unimplemented!() })
     }
 }
 impl Player for Team {
     type DeserializedType = Self;
     fn add_player(&self, db_pool: &SqlitePool) -> AsyncDbRes {
-        Box::pin(async move {})
-            unimplemented!()
+        Box::pin(async move { unimplemented!() })
     }
     fn update_score(&self, db_pool: &SqlitePool) -> AsyncDbRes {
-        Box::pin(async move {})
-            unimplemented!()
+        Box::pin(async move { unimplemented!() })
     }
 }
