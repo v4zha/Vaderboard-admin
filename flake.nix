396bdaaa7b45f4ad93933a097dfe929814843e7d
arvin <arvin_j4848@protonmail.com>
2023-08-02 12:28:26 +0530

    feat:nix flake

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "v4zha's VaderBoard Admin flakes";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        rust-toolchain = pkgs.rust-bin.selectLatestNightlyWith
+          (toolchain: toolchain.default.override {
+            extensions = [ "rust-src" ];
+          });
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          buildInputs = [
+            rust-toolchain
+            rust-analyzer
+            lldb
+            mold
+            taplo
+            yaml-language-server
+            nodejs
+            typescript-language-server
+          ];
+        };
+      }
+    );
+}
